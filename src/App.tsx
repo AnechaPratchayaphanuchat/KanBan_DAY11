a5a8b8aba8468b20f70049318ed55e090060366e
Anacha Pratchayphanuchat <luzus123@gmail.com>
2025-05-19 12:59:02 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Login from "./Login";
+import Form from "./Form";
+import axios from "axios";
+
+// Recommended
+function App() {
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.escuelajs.co/api/v1/products")
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1
+        id="title"
+        style={{
+          color: "navy",
+          paddingBottom: "20px",
+          fontSize: "100px",
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+          textAlign: "center",
+          borderRadius: "10px",
+        }}
+      >
+        Sunny Shop 101
+      </h1>
+
+      <div className="product-grid" style={{ paddingTop:"20px" }}>
+        {products.map((product) => (
+          <div className="product-card">
+            <h1 style={{ fontSize: "35px", paddingBottom: "15px" }}>{product.title}</h1>
+            <p style={{ fontSize: "15px" }}>{product.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
